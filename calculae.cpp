0b42a78b3a4d5f6578baa9e5f99f2bd212b16ae0
Theeraphat-CH <124338336+Theeraphat-CH@users.noreply.github.com>
2023-03-02 12:55:44 +0700

    Create calculae.cpp

---
+++ calculae.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int x, y;
+
+    std::cout << "Enter number : ";
+    std::cin >> x;
+
+    std::cout << "Enter number : ";
+    std::cin >> y;
+
+    std::cout << "Result : " << x+y << std::endl;
+}
